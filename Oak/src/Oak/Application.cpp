31f15017f2d78a3963d7434105ab6206d0f5fb23
Ultamatum <alexanderg0502@gmail.com>
2020-05-18 13:36:01 +0100

    Started Abstraction of Vertex Buffer Layouts

--- Oak/src/Oak/Application.cpp
+++ Oak/src/Oak/Application.cpp
@@ -11,6 +11,27 @@ namespace Oak
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Oak::ShaderDataType::Float:	return GL_FLOAT;
+			case Oak::ShaderDataType::Float2:	return GL_FLOAT;
+			case Oak::ShaderDataType::Float3:	return GL_FLOAT;
+			case Oak::ShaderDataType::Float4:	return GL_FLOAT;
+			case Oak::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Oak::ShaderDataType::Mat4:		return GL_FLOAT;
+			case Oak::ShaderDataType::Int:		return GL_INT;
+			case Oak::ShaderDataType::Int2:		return GL_INT;
+			case Oak::ShaderDataType::Int3:		return GL_INT;
+			case Oak::ShaderDataType::Int4:		return GL_INT;
+			case Oak::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		OK_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		//Makes sure there isn't a duplicate application being created
@@ -29,16 +50,34 @@ namespace Oak
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Colour" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		for (const auto& element : m_VertexBuffer->GetLayout())
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				m_VertexBuffer->GetLayout().GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		unsigned int indices[3] = {0, 1, 2};
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -47,12 +86,15 @@ namespace Oak
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Colour;
 
 			out vec3 v_Position;
+			out vec4 v_Colour;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Colour = a_Colour;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 
@@ -64,10 +106,11 @@ namespace Oak
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Colour;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Colour;
 			}
 
 		)";
