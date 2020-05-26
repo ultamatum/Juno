db13c5243062111c6b94c30e61c0dd4e85318740
Ultamatum <alexanderg0502@gmail.com>
2020-05-26 23:44:30 +0100

    Camera Controller Improvements
    Camera now moves, dependant on the way it's rotated (w is local up, not global)

--- Juno/src/Juno/Renderer/CameraController.cpp
+++ Juno/src/Juno/Renderer/CameraController.cpp
@@ -14,22 +14,39 @@ namespace Juno
 
 	void OrthographicCameraController::OnUpdate(Timestep ts)
 	{
-		if (Input::IsKeyPressed(JUNO_KEY_A))
-			m_CameraPosition.x -= m_CameraTranslationSpeed * ts;
-		else if (Input::IsKeyPressed(JUNO_KEY_D))
-			m_CameraPosition.x += m_CameraTranslationSpeed * ts;
+		if (Input::IsKeyPressed(JUNO_KEY_A)) 
+		{
+			m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+		}
+		else if (Input::IsKeyPressed(JUNO_KEY_D)) 
+		{
+			m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+		}
 
 		if (Input::IsKeyPressed(JUNO_KEY_W))
-			m_CameraPosition.y += m_CameraTranslationSpeed * ts;
-		else if (Input::IsKeyPressed(JUNO_KEY_S))
-			m_CameraPosition.y -= m_CameraTranslationSpeed * ts;
+		{
+			m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+		}
+		else if (Input::IsKeyPressed(JUNO_KEY_S)) 
+		{
+			m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+			m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
+		}
 
 		if (m_Rotation)
 		{
 			if (Input::IsKeyPressed(JUNO_KEY_E))
 				m_CameraRotation -= m_CameraRotationSpeed * ts;
-			else if (Juno::Input::IsKeyPressed(JUNO_KEY_Q))
+			if (Juno::Input::IsKeyPressed(JUNO_KEY_Q))
 				m_CameraRotation += m_CameraRotationSpeed * ts;
+
+			if (m_CameraRotation > 180.0f)
+				m_CameraRotation -= 360.0f;
+			else if (m_CameraRotation <= -180.0f)
+				m_CameraRotation += 360.0f;
 
 			m_Camera.SetRotation(m_CameraRotation);
 		}
