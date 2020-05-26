08de84744408e1814875604ad10f2f84913d5f27
Ultamatum <alexanderg0502@gmail.com>
2020-05-26 23:28:42 +0100

    Check OpenGL version

--- Juno/src/Platform/OpenGL/OpenGLContext.cpp
+++ Juno/src/Platform/OpenGL/OpenGLContext.cpp
@@ -22,6 +22,17 @@ namespace Juno
 		JUNO_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
 		JUNO_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		JUNO_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+		#ifdef JUNO_ENABLE_ASSERTS
+			int versionMajor;
+            int versionMinor;
+            glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+            glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+			JUNO_CORE_ASSERT(
+                versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5),
+                "Juno requires at least OpenGL version 4.5!");
+		#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
