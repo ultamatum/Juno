1feb89d71fcf27bf571b2aff3144982b05477bdc
Ultamatum <alexanderg0502@gmail.com>
2020-06-02 13:43:42 +0100

    Fixed Renderer2D Bug
    Fixed a bug wherein you couldn't render nothing.

--- Juno/src/Juno/Renderer/Renderer2D.cpp
+++ Juno/src/Juno/Renderer/Renderer2D.cpp
@@ -136,6 +136,9 @@ namespace Juno
 	{
 		JUNO_PROFILE_FUNCTION();
 
+		if (s_Data.QuadIndexCount == 0)
+			return;	//Nothing to draw
+
 		for (uint32_t i = 0; i < s_Data.TextureSlotIndex; i++)
 			s_Data.TextureSlots[i]->Bind(i);
 
