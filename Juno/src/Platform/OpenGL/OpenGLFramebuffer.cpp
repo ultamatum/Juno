505c090f10dafec2029c9eca5b0b2cef9635a58e
Ultamatum <alexanderg0502@gmail.com>
2020-08-05 17:53:15 +0100

    Fixed framebuffer underflow error

--- Juno/src/Platform/OpenGL/OpenGLFramebuffer.cpp
+++ Juno/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -5,6 +5,8 @@
 
 namespace Juno
 {
+	static const uint32_t s_MaxFramebufferSize = 16384;
+
 	OpenGLFrameBuffer::OpenGLFrameBuffer(const FramebufferSpecification& spec)
 		:m_Specification(spec)
 	{
@@ -61,6 +63,12 @@ namespace Juno
 
 	void OpenGLFrameBuffer::Resize(uint32_t width, uint32_t height)
 	{
+		if (width == 0 || height == 0 || width > s_MaxFramebufferSize || height > s_MaxFramebufferSize)
+		{
+			JUNO_CORE_WARN("Attempted to resize framebuffer to {0}, {1}", width, height);
+			return;
+		}
+
 		m_Specification.Width = width;
 		m_Specification.Height = height;
 
