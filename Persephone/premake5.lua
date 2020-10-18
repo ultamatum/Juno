aacaa36b00a074fbd4090363533cb1a4e8774786
Ultamatum <alexanderg0502@gmail.com>
2020-10-18 19:30:56 +0100

    Modularised premake files

---
+++ Persephone/premake5.lua
@@ -0,0 +1,46 @@
+project "Persephone"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{wks.location}/Juno/vendor/spdlog/include",
+		"%{wks.location}/Juno/src",
+		"%{wks.location}/Juno/vendor",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"Juno"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "JUNO_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "JUNO_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "JUNO_DIST"
+		runtime "Release"
+		optimize "on"
--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+
 workspace "Juno"
 	architecture "x64"
 	startproject "Sandbox"
@@ -9,6 +11,11 @@ workspace "Juno"
 		"Dist"
 	}
 
+	solution_items
+	{
+		".editorconfig"
+	}
+
 	flags
 	{
 		"MultiProcessorCompile"
@@ -18,187 +25,20 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root
 IncludeDir = {}
-IncludeDir["GLFW"] = "Juno/vendor/GLFW/include"
-IncludeDir["Glad"] = "Juno/vendor/Glad/include"
-IncludeDir["ImGui"] = "Juno/vendor/imgui"
-IncludeDir["glm"] = "Juno/vendor/glm"
-IncludeDir["stb_image"] = "Juno/vendor/stb_image"
-IncludeDir["entt"] = "Juno/vendor/EnTT/include"
+IncludeDir["GLFW"] = "%{wks.location}/Juno/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/Juno/vendor/Glad/include"
+IncludeDir["ImGui"] = "%{wks.location}/Juno/vendor/imgui"
+IncludeDir["glm"] = "%{wks.location}/Juno/vendor/glm"
+IncludeDir["stb_image"] = "%{wks.location}/Juno/vendor/stb_image"
+IncludeDir["entt"] = "%{wks.location}/Juno/vendor/EnTT/include"
 
 group "Dependencies"
+	include "vendor/premake"
 	include "Juno/vendor/GLFW"
 	include "Juno/vendor/Glad"
 	include "Juno/vendor/imgui"
 group ""
 
-project "Juno"
-	location "Juno"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "junopch.h"
-	pchsource "Juno/src/junopch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.h",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl"
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"JUNO_BUILD_DLL",
-			"GLFW_INCLUDE_NONE"
-		}
-
-		links
-		{
-			"opengl32.lib"
-		}
-
-	filter "configurations:Debug"
-		defines "JUNO_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "JUNO_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "JUNO_DIST"
-		runtime "Release"
-		optimize "on"
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-	}
-
-	includedirs
-	{
-		"Juno/vendor/spdlog/include",
-		"Juno/src",
-		"Juno/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"Juno"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		
-	filter "configurations:Debug"
-		defines "JUNO_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "JUNO_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "JUNO_DIST"
-		runtime "Release"
-		optimize "on"
-
-project "Persephone"
-	location "Persephone"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-	}
-
-	includedirs
-	{
-		"Juno/vendor/spdlog/include",
-		"Juno/src",
-		"Juno/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"Juno"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		defines "JUNO_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "JUNO_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "JUNO_DIST"
-		runtime "Release"
-		optimize "on"
+include "Juno"
+include "Sandbox"
+include "Persephone"
