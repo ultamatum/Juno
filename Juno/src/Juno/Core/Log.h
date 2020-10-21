7262ac35f91ed265ea70ec26dcc4716e61105ef3
Ultamatum <alexanderg0502@gmail.com>
2020-10-21 15:38:12 +0100

    Hide compiler warnings from SPDLog

--- Juno/src/Juno/Core/Log.h
+++ Juno/src/Juno/Core/Log.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "Core.h"
+
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
+#pragma warning(pop)
 
 namespace Juno
 {
